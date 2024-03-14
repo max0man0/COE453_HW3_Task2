3034de44825a47ba145bddb850bd14ba9f5104a8
max0man0 <0naxman0@gmail.com>
2024-03-14 21:28:17 +0300

    init

---
+++ code/add.py
@@ -0,0 +1,20 @@
+import functions_framework
+from flask import jsonify
+
+@functions_framework.http
+def add(request):
+    """HTTP Cloud Function.
+    Args:
+        request (flask.Request): The request object.
+        <https://flask.palletsprojects.com/en/1.1.x/api/#incoming-request-data>
+    Returns:
+        The sum of the provided numbers
+        <https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response>.
+    """
+    request_json = request.get_json(silent=True)
+
+    x = request_json['X']
+    y = request_json['Y']
+    sum = x + y
+    result = jsonify({'X': x, 'Y': y, 'Result': sum})
+    return result
